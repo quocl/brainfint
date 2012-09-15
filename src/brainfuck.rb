56cd7e5ad1a9e3fad6c09977dbc6ad5fb270cf68
quocle <quocle@some_examples.org>
2012-09-15 02:15:36 -0400

    add increase, decrease, right and left

--- src/brainfuck.rb
+++ src/brainfuck.rb
@@ -1,9 +1,11 @@
 # Brainfuck interpreter in ruby
 
 class Brainfuck
+	SIZE = 30000
 	def initialize
 		@VALID_COMMANDS = ['>', '<', '+', '-', '.',',', '[', ']']
-		@limit = [0]*30000
+		@limit = Array.new(SIZE,0)
+		@pos = 0
 	end
 	def self.abc
 		puts 'hello'
@@ -17,10 +19,27 @@ class Brainfuck
 		@limit=val
 	end
 
+	def move_right
+		@pos += 1 unless @pos >= SIZE - 1
+	end
+
+	def move_left
+		@pos -= 1 unless @pos <= 0
+	end 
+
+	def increase
+		@limit[@pos] += 1 
+	end
+	
+	def decrease
+		@limit[@pos] -= 1
+	end
+
+
+
 end
 
 #Brainfuck.abc
 a = Brainfuck.new
 puts a.limit
-a.limit = 4
-puts a.limit
+
